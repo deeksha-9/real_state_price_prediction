06551646a7aef305b864a33b0041a94ff209024e
Deeksha <deekshapriya9@gmail.com>
2021-01-12 19:40:00 +0530

    added app.html

--- flask/server/server.py
+++ flask/server/server.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 @app.route('/get_location_names')
 def get_location(): 
     response = jsonify({
-       'location' :util.get_locatio_names()
+       'location' :util.get_location_names()
        })
     response.headers.add('Access-control-Allow-Origin', '*')
     return response
@@ -21,7 +21,7 @@ def get_location():
 @app.route('/predict_home_price',methods =['POST'])
 def predict_home_price():
     total_sqft = float(request.form['total_sqft'])
-    location = request.form['total_sqft']
+    location = request.form['location']
     bhk = int(request.form['bhk'])
     bath = int(request.form['bath']) 
 
